7050a24d69f2cfbcba607ce553ebd7cd629a66fa
wang4537 <57774968+wang4537@users.noreply.github.com>
2025-11-11 11:48:10 -0500

    Add files via upload

---
+++ LSTM_pytorch.py
@@ -0,0 +1,98 @@
+#import libraries
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+class LSTM(torch.nn.Module):
+    def __init__(self,n_features,seq_length):
+        super(MV_LSTM, self).__init__()
+        self.n_features = n_features
+        self.seq_len = seq_length
+        self.n_hidden = 20 # number of hidden states
+        self.n_layers = 1 # number of LSTM layers (stacked)
+
+        self.l_lstm = torch.nn.LSTM(input_size = n_features,
+                                 hidden_size = self.n_hidden,
+                                 num_layers = self.n_layers,
+                                 batch_first = True)
+        # according to pytorch docs LSTM output is
+        # (batch_size,seq_len, num_directions * hidden_size)
+        # when considering batch_first = True
+        self.l_linear = torch.nn.Linear(self.n_hidden*self.seq_len, 1)
+
+
+    def init_hidden(self, batch_size):
+        # even with batch_first = True this remains same as docs
+        hidden_state = torch.zeros(self.n_layers,batch_size,self.n_hidden)
+        cell_state = torch.zeros(self.n_layers,batch_size,self.n_hidden)
+        self.hidden = (hidden_state, cell_state)
+
+
+    def forward(self, x):
+        batch_size, seq_len, _ = x.size()
+
+        lstm_out, self.hidden = self.l_lstm(x,self.hidden)
+        # lstm_out(with batch_first = True) is
+        # (batch_size,seq_len,num_directions * hidden_size)
+        # for following linear layer we want to keep batch_size dimension and merge rest
+        # .contiguous() -> solves tensor compatibility error
+        x = lstm_out.contiguous().view(batch_size,-1)
+        return self.l_linear(x)
+
+# See what the scores are before training
+# Note that element i,j of the output is the score for tag j for word i.
+# Here we don't need to train, so the code is wrapped in torch.no_grad()
+with torch.no_grad():
+    inputs = prepare_sequence(training_data[0][0], word_to_ix)
+    tag_scores = model(inputs)
+    print(tag_scores)
+
+for epoch in range(300):  # again, normally you would NOT do 300 epochs, it is toy data
+    for sentence, tags in training_data:
+        # Step 1. Remember that Pytorch accumulates gradients.
+        # We need to clear them out before each instance
+        model.zero_grad()
+
+        # Step 2. Get our inputs ready for the network, that is, turn them into
+        # Tensors of word indices.
+        sentence_in = prepare_sequence(sentence, word_to_ix)
+        targets = prepare_sequence(tags, tag_to_ix)
+
+        # Step 3. Run our forward pass.
+        tag_scores = model(sentence_in)
+
+        # Step 4. Compute the loss, gradients, and update the parameters by
+        #  calling optimizer.step()
+        loss = loss_function(tag_scores, targets)
+        loss.backward()
+        optimizer.step()
+
+# See what the scores are after training
+with torch.no_grad():
+    inputs = prepare_sequence(training_data[0][0], word_to_ix)
+    tag_scores = model(inputs)
+
+    # The sentence is "the dog ate the apple".  i,j corresponds to score for tag j
+    # for word i. The predicted tag is the maximum scoring tag.
+    # Here, we can see the predicted sequence below is 0 1 2 0 1
+    # since 0 is index of the maximum value of row 1,
+    # 1 is the index of maximum value of row 2, etc.
+    # Which is DET NOUN VERB DET NOUN, the correct sequence!
+    print(tag_scores)
+
+        self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
+
+        # The LSTM takes word embeddings as inputs, and outputs hidden states
+        # with dimensionality hidden_dim.
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim)
+
+        # The linear layer that maps from hidden state space to tag space
+        self.hidden2tag = nn.Linear(hidden_dim, tagset_size)
+
+    def forward(self, sentence):
+        embeds = self.word_embeddings(sentence)
+        lstm_out, _ = self.lstm(embeds.view(len(sentence), 1, -1))
+        tag_space = self.hidden2tag(lstm_out.view(len(sentence), -1))
+        tag_scores = F.log_softmax(tag_space, dim=1)
+        return tag_scores
